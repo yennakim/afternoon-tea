856a5126a71f367e20ab224701af68aa8ba1233f
Daun Kim <yennakim11@gmail.com>
2024-02-21 22:02:33 -0600

    added delete food item JS promise to complete CRUD

--- api/foodData.js
+++ api/foodData.js
@@ -56,9 +56,23 @@ const updateFood = (payload) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+// DELETE FOOD
+const deleteSingleFood = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/foods/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
 export {
   getFoods,
   getSingleFood,
   createFood,
   updateFood,
+  deleteSingleFood,
 };
