5422ba15c5d2a836d6eb7bac0d73a87c2bacfd91
Daun Kim <yennakim11@gmail.com>
2024-02-25 21:20:23 -0600

    merged data between teas and foods for view details pages

---
+++ api/mergedData.js
@@ -0,0 +1,26 @@
+import { getFoodsPairedWithTea, getSingleTea } from './teaData';
+import { getSingleFood } from './foodData';
+
+const viewFoodDetails = (foodFirebaseKey) => new Promise((resolve, reject) => {
+  getSingleFood(foodFirebaseKey)
+    .then((foodObject) => {
+      getSingleTea(foodObject.teaId)
+        .then((teaObject) => {
+          resolve({ teaObject, ...foodObject });
+        });
+    }).catch((error) => reject(error));
+});
+
+const viewTeaDetails = (teaFirebaseKey) => new Promise((resolve, reject) => {
+  Promise.all([getSingleTea(teaFirebaseKey),
+    getFoodsPairedWithTea(teaFirebaseKey)])
+    .then(([teaObject, FoodsForTeaArray]) => {
+      resolve({
+        ...teaObject,
+        foods:
+      FoodsForTeaArray,
+      });
+    }).catch((error) => reject(error));
+});
+
+export { viewFoodDetails, viewTeaDetails };
