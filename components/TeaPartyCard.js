a52d491d07459764566d86e8deb4ad94e225169f
Daun Kim <yennakim11@gmail.com>
2024-03-17 00:22:55 -0500

    rendering created by: user display name on teaParty cards

--- components/TeaPartyCard.js
+++ components/TeaPartyCard.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import Link from 'next/link';
 import { deleteSingleTeaParty } from '../api/teaPartyData';
+import { useAuth } from '../utils/context/authContext';
 
 export default function TeaPartyCard({ teaPartyObj, onUpdate }) {
+  const { user } = useAuth();
+
   const deleteTeaParty = () => {
     if (window.confirm(`Would you like to delete ${teaPartyObj.name}?`)) {
       deleteSingleTeaParty(teaPartyObj.firebaseKey).then(() => onUpdate());
@@ -16,6 +19,7 @@ export default function TeaPartyCard({ teaPartyObj, onUpdate }) {
       <Card.Img variant="top" src={teaPartyObj.image} alt={teaPartyObj.name} style={{ height: '400px' }} />
       <Card.Body>
         <Card.Title>{teaPartyObj.name}</Card.Title>
+        <Card.Text>Created by: {user.uid === teaPartyObj.uid ? `${user.displayName}` : 'Anonymous' }</Card.Text>
         {/* DYNAMIC LINK TO EDIT THE TEA DETAILS  */}
         <Link href={`/teaParty/${teaPartyObj.firebaseKey}`} passHref>
           <Button variant="primary" className="m-2">VIEW</Button>
@@ -36,6 +40,7 @@ TeaPartyCard.propTypes = {
     image: PropTypes.string,
     name: PropTypes.string,
     firebaseKey: PropTypes.string,
+    uid: PropTypes.string,
   }).isRequired,
   onUpdate: PropTypes.func.isRequired,
 };
